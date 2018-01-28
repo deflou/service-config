da28aaa58bf8ed21b898587eb5b91939667f918d
deflou <35891055+deflou@users.noreply.github.com>
2018-01-28 21:56:28 +0300

    Create df.dist.php

---
+++ resources/df.dist.php
@@ -0,0 +1,83 @@
+<?php
+
+return [
+  'service' => [
+    'name' => '',
+    'title' => '',
+    'description' => '',
+    'resolver' => '',
+    'version' => '',
+    'authors' => [
+      [
+        'name' => '',
+        'url' => '',
+        'email' => ''
+      ]
+    ],
+    'options' => [
+      [
+        'name' => '',
+        'title' => '',
+        'description' => ''
+      ]
+    ],
+    'events' => [
+      [
+        'name' => '',
+        'title' => '',
+        'description' => '',
+        'parameters' => [
+          [
+            'name' => '',
+            'title' => '',
+            'description' => '',
+            'value' => '',
+            'compares' => [
+              [
+                'name' => '',
+                'title' => '',
+                'description' => '',
+                'class' => ''
+              ]
+            ]
+          ]
+        ],
+        'pilers' => [
+          [
+            'class' => '',
+            'options' => []
+          ]
+        ]
+      ]
+    ],
+    'actions' => [
+      [
+        'name' => '',
+        'title' => '',
+        'description' => '',
+        'parameters' => [
+          [
+            'name' => '',
+            'title' => '',
+            'description' => '',
+            'value' => '',
+            'compares' => [
+              [
+                'name' => '',
+                'title' => '',
+                'description' => '',
+                'class' => ''
+              ]
+            ]
+          ]
+        ],
+        'pilers' => [
+          [
+            'class' => '',
+            'options' => []
+          ]
+        ]
+      ]
+    ]
+  ]
+];
